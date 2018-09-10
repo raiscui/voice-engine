c5787018241b3969396d691c6c0ca8d4e7025e98
RaisCui <vdcoolzi@gmail.com>
2018-09-10 15:04:41 +0800

    fix2

--- voice_engine/pulseaudio_source.py
+++ voice_engine/pulseaudio_source.py
@@ -3,6 +3,7 @@ import time
 import numpy
 import datetime
 import threading
+import logging
 
 from voice_engine.element import Element
 
@@ -15,6 +16,8 @@ default_mic = sc.default_microphone()
 class Source(Element):
     def __init__(self, rate=16000, frames_size=1280, channels=1, device_name='default', bits_per_sample=16):
         super(Source, self).__init__()
+        self.logger = logging.getLogger(self.__class__.__name__)
+
         self.mic = None
         self.rate = rate
         self.channels = channels
@@ -26,11 +29,17 @@ class Source(Element):
             try:
                 self.mic = sc.get_microphone(device_name)
             except Exception:
+                self.logger.warn("can't get by device_name")
                 self.mic = sc.default_microphone()
+        else:
+            self.mic = sc.default_microphone()
 
     def run(self):
+        self.logger.warn('in run')
+
         try:
             with self.mic.recorder(samplerate=self.rate, channels=self.channels) as mic:
+                self.logger.warn('record now ')
                 while not self.done:
                     data = mic.flush()
                     if len(data):
@@ -41,10 +50,11 @@ class Source(Element):
                         super(Source, self).put(data)
 
         except Exception as e:
-            print(e)
+            self.logger.error(e)
 
     def start(self):
         """Start a recording thread"""
+        self.logger.warn('start')
         self.done = False
         self.thread = threading.Thread(target=self.run)
         self.thread.daemon = True
@@ -52,6 +62,8 @@ class Source(Element):
 
     def stop(self):
         """Stop recording"""
+        self.logger.warn('stop')
+
         self.done = True
         if self.thread and self.thread.is_alive():
             self.thread.join(timeout=3)
